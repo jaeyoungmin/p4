f26d25a952d6654e27fd3062c56619f0a06ee3da
jaeyoungmin <jaeyoung8456@naver.com>
2020-11-12 17:29:41 +0900

    dd

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,91 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="cursor"></div>
+
+    <CoreAppBar />
+    <CoreView />
+    <CoreFooter />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld
-  }
+    CoreAppBar: () => import('@/components/core/CoreAppBar.vue'),
+    CoreView: () => import('@/components/core/CoreView.vue'),
+    CoreFooter: () => import('@/components/core/CoreFooter.vue'),
+  },
+  mounted() {
+    const cursor = document.querySelector('.cursor');
+    document.addEventListener('mousemove', (e) => {
+      cursor.setAttribute(
+        'style',
+        'top: ' + (e.pageY - 10) + 'px; left:' + (e.pageX - 10) + 'px'
+      );
+    });
+
+    document.addEventListener('click', () => {
+      cursor.classList.add('expand');
+
+      setTimeout(() => {
+        cursor.classList.remove('expand');
+      }, 500);
+    });
+  },
 };
 </script>
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  font-family: consolas;
+  box-sizing: border-box;
+  cursor: none;
+}
+.home {
+  min-height: 100vh;
+  background-color: #050801;
+}
+.cursor {
+  width: 20px;
+  height: 20px;
+  border: 1px solid white;
+  border-radius: 50%;
+  position: absolute;
+  transition-duration: 200ms;
+  transition-timing-function: ease out;
+  animation: cursorAnim 0.5s infinite alternate;
+}
+
+.expand {
+  animation: cursorAnim3 0.5s forwards;
+  border: 1px solid#03e9f4;
+}
+
+@keyframes cursorAnim {
+  from {
+    transform: scale(1);
+  }
+
+  to {
+    transform: scale(0.7);
+  }
+}
+
+@keyframes cursorAnim3 {
+  0% {
+    transform: scale(1);
+  }
+
+  50% {
+    transform: scale(2);
+  }
+
+  100% {
+    tranform: scale(1);
+  }
+}
+</style>

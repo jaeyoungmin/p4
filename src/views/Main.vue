a89a74098437273e3d2544931969b76f4ebad4c1
jaeyoungmin <jaeyoung8456@naver.com>
2020-11-13 14:06:21 +0900

    typer

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,221 +1,107 @@
 <template>
-  <v-row class="main d-flex justify-center align-center">
-    <div class="main-item">
-      M
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-    <div class="main-item">
-      I
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-    <div class="main-item">
-      N
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-    <div class="main-item">
-      J
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-    <div class="main-item">
-      A
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-    <div class="main-item">
-      E
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-    <div class="main-item">
-      Y
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-    <div class="main-item">
-      O
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-    <div class="main-item">
-      U
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-    <div class="main-item">
-      N
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-    <div class="main-item">
-      G
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-  </v-row>
+  <div class="container">
+    <h1>
+      나는
+      <span class="typed-text">{{ typeValue }}</span>
+      다다다
+      <span class="cursor" :class="{ typing: typeStatus }">&nbsp;</span>
+    </h1>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Main',
+  data() {
+    return {
+      typeValue: '',
+      typeStatus: false,
+      typeArray: ['집에가고싶다', '배고프다', '졸리다', '으아아아악'],
+      typingSpeed: 200,
+      erasingSpeed: 100,
+      newTextDelay: 2000,
+      typeArrayIndex: 0,
+      charIndex: 0,
+    };
+  },
+  methods: {
+    typeText() {
+      if (this.charIndex < this.typeArray[this.typeArrayIndex].length) {
+        if (!this.typeStatus) this.typeStatus = true;
+        this.typeValue += this.typeArray[this.typeArrayIndex].charAt(
+          this.charIndex
+        );
+        this.charIndex += 1;
+        setTimeout(this.typeText, this.typingSpeed);
+      } else {
+        this.typeStatus = false;
+        setTimeout(this.eraseText, this.newTextDelay);
+      }
+    },
+    eraseText() {
+      if (this.charIndex > 0) {
+        if (!this.typeStatus) this.typeStatus = true;
+        this.typeValue = this.typeArray[this.typeArrayIndex].substring(
+          0,
+          this.charIndex - 1
+        );
+        this.charIndex -= 1;
+        setTimeout(this.eraseText, this.erasingSpeed);
+      } else {
+        this.typeStatus = false;
+        this.typeArrayIndex += 1;
+        if (this.typeArrayIndex >= this.typeArray.length) {
+          this.typeArrayIndex = 0;
+          setTimeout(this.typeText, this.typingSpeed + 1000);
+        }
+      }
+    },
+  },
+  created() {
+    setTimeout(this.typeText, this.newTextDelay + 200);
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.main {
+.container {
+  width: 100%;
   height: 100vh;
-}
-.main-item {
-  position: relative;
   display: flex;
-  align-items: center;
-  margin: 10px;
   justify-content: center;
-  padding: 5px 10px;
-  color: #03e9f4;
-  font-size: 24px;
-  text-decoration: none;
-  text-transform: uppercase;
-  transition: 0.5s;
-  letter-spacing: 4px;
-  overflow: hidden;
-  -webkit-box-reflect: below 1px linear-gradient(transparent, #0005);
+  align-items: center;
 }
+h1 {
+  font-size: 4rem;
+  color: #fff;
+  font-weight: normal;
 
-.main-item:hover {
-  background: #03e9f4;
-  color: #050801;
-  box-shadow: 0 0 5px #03e9f4, 0 0 25px #03e9f4, 0 0 50px #03e9f4,
-    0 0 100px #03e9f4;
-}
-.main-item:nth-child(1) {
-  filter: hue-rotate(290deg);
-}
-.main-item:nth-child(3) {
-  filter: hue-rotate(110deg);
-}
-.main-item:nth-child(4) {
-  filter: hue-rotate(10deg);
-}
-.main-item:nth-child(5) {
-  filter: hue-rotate(80deg);
-}
-.main-item:nth-child(6) {
-  filter: hue-rotate(180deg);
-}
-.main-item:nth-child(7) {
-  filter: hue-rotate(120deg);
-}
-.main-item:nth-child(8) {
-  filter: hue-rotate(0deg);
-}
-.main-item:nth-child(9) {
-  filter: hue-rotate(80deg);
-}
-.main-item:nth-child(10) {
-  filter: hue-rotate(270deg);
-}
-.main-item:nth-child(11) {
-  filter: hue-rotate(150deg);
-}
-.main-item span {
-  position: absolute;
-  display: block;
-}
-.main-item span:nth-child(1) {
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 2px;
-  background: linear-gradient(90deg, transparent, #03e9f4);
-  animation: animate1 1s linear infinite;
-}
-@keyframes animate1 {
-  0% {
-    left: -100%;
+  span.typed-text {
+    color: #fff;
+    font-size: 4rem;
   }
-  50%,
-  100% {
-    left: 100%;
+  span.cursor {
+    display: inline-block;
+    margin: 10px;
+    width: 2px;
+    border: 0;
+    height: 4rem;
+    background-color: #fff;
+    animation: cursorBlink 1s infinite;
+  }
+  span.cursor.typing {
+    animation: none;
   }
 }
-.main-item span:nth-child(2) {
-  top: -100%;
-  right: 0;
-  width: 2px;
-  height: 100%;
-  background: linear-gradient(180deg, transparent, #03e9f4);
-  animation: animate2 1s linear infinite;
-  animation-delay: 0.25s;
-}
-@keyframes animate2 {
-  0% {
-    top: -100%;
+@keyframes cursorBlink {
+  49% {
+    background-color: #fff;
   }
-  50%,
-  100% {
-    top: 100%;
+  50% {
+    background-color: transparent;
   }
-}
-.main-item span:nth-child(3) {
-  bottom: 0;
-  right: -100%;
-  width: 100%;
-  height: 2px;
-  background: linear-gradient(270deg, transparent, #03e9f4);
-  animation: animate3 1s linear infinite;
-  animation-delay: 0.5s;
-}
-@keyframes animate3 {
-  0% {
-    right: -100%;
-  }
-  50%,
-  100% {
-    right: 100%;
-  }
-}
-.main-item span:nth-child(4) {
-  bottom: -100%;
-  left: 0;
-  width: 2px;
-  height: 100%;
-  background: linear-gradient(270deg, transparent, #03e9f4);
-  animation: animate4 1s linear infinite;
-  animation-delay: 0.75s;
-}
-@keyframes animate4 {
-  0% {
-    bottom: -100%;
-  }
-  50%,
-  100% {
-    bottom: 100%;
+  99% {
+    background-color: transparent;
   }
 }
 </style>
